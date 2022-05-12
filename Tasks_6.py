8c4cf069d855baa8367cbd252d9667f176ef219d
sidorenko_ml <sidorenkooa@mail.ru>
2022-05-12 17:58:13 +0300

    Найден минимальный элемент матрицы

--- Tasks_6.py
+++ Tasks_6.py
@@ -1,5 +1,6 @@
 from random import randint
 n = 3
+print("Дана матрица случайных чисел:")
 Matrica = [[randint(1,40) for i in range(n)] for item in range(n)]
 print(Matrica)
 MaxElem = []
@@ -7,3 +8,4 @@ for item in Matrica:
     for i in item:
         MaxElem.append(i)
 print("Максимальный элемент матрицы - ",max(MaxElem))
+print("Минимальный элемент матрицы - ",min(MaxElem))
