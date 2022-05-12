9c35747cdbd878fff3afb1c67a711501f5cca4bf
sidorenko_ml <sidorenkooa@mail.ru>
2022-05-12 17:54:56 +0300

    Найден максимальный элемент матрицы

--- Tasks_6.py
+++ Tasks_6.py
@@ -2,3 +2,8 @@ from random import randint
 n = 3
 Matrica = [[randint(1,40) for i in range(n)] for item in range(n)]
 print(Matrica)
+MaxElem = []
+for item in Matrica:
+    for i in item:
+        MaxElem.append(i)
+print("Максимальный элемент матрицы - ",max(MaxElem))
