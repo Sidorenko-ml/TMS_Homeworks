9a2f639987cac7f7ffa9b36ccf329fd66bf99a84
sidorenko_ml <sidorenkooa@mail.ru>
2022-05-12 19:38:31 +0300

    Найдена колонка с максимальной суммой элементов

--- Tasks_6.py
+++ Tasks_6.py
@@ -1,6 +1,7 @@
 import enum
 from operator import index
 from random import randint
+from xml.dom.expatbuilder import theDOMImplementation
 from xmlrpc.client import Marshaller
 n = 3
 print("Дана матрица случайных чисел:")
@@ -19,3 +20,8 @@ for item in Matrica:
     sum_of_row.append(sum(item))
 print(sum_of_row)
 print("Индекс ряда с максимальной суммой элементов - ", sum_of_row.index(max(sum_of_row)))
+one_col = [Matrica[0][0],Matrica[1][0],Matrica[2][0]]
+second_col = [Matrica[0][1],Matrica[1][1],Matrica[2][1]]
+three_col = [Matrica[0][2],Matrica[1][2],Matrica[2][2]]
+column = [sum(one_col),sum(second_col),sum(three_col)]
+print("Индекс колонок с максимальной суммой элементов - ", column.index(max(column)))
