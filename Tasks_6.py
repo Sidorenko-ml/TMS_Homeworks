69355ac3dd97e9f159ebc42e738ca08802605491
sidorenko_ml <sidorenkooa@mail.ru>
2022-05-12 19:00:51 +0300

    Найден индекс ряда с максимальной суммой элементов

--- Tasks_6.py
+++ Tasks_6.py
@@ -1,4 +1,7 @@
+import enum
+from operator import index
 from random import randint
+from xmlrpc.client import Marshaller
 n = 3
 print("Дана матрица случайных чисел:")
 Matrica = [[randint(1,40) for i in range(n)] for item in range(n)]
@@ -11,3 +14,8 @@ print("Максимальный элемент матрицы - ",max(MaxElem))
 print("Минимальный элемент матрицы - ",min(MaxElem))
 Sum_of_elements = sum(MaxElem)
 print('Сумма всех элементов - ',Sum_of_elements)
+sum_of_row = []
+for item in Matrica:
+    sum_of_row.append(sum(item))
+print(sum_of_row)
+print("Индекс ряда с максимальной суммой элементов - ", sum_of_row.index(max(sum_of_row)))
